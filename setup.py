4e532bc0b66a63fdbe4650df94b08256123b0f18
moriaki3193 <puppis1221@gmail.com>
2018-02-11 16:58:58 +0900

    modify setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,19 @@ from setuptools import setup, find_packages, Extension
 from Cython.Distutils import build_ext
 import numpy
 
-setup(
-	maintainer='Corey Lynch',
-    name='pyfm',
-    packages=find_packages(),
-    url='https://github.com/coreylynch/pyFM',
-    cmdclass = {'build_ext': build_ext},
-    ext_modules = [Extension("pyfm_fast", ["pyfm_fast.pyx"],
-    						 libraries=["m"],
-    						 include_dirs=[numpy.get_include()])]
-)
+
+ext_modules = [
+    Extension("pyfm_fast",
+              ["pyfm_fast.pyx"],
+              libraries=["m"],
+              include_dirs=[numpy.get_include()])
+]
+
+
+setup(maintainer='Moriaki Saigusa',
+      name='pyfm',
+      packages=find_packages(),
+      url='https://github.com/moriaki3193/pyFM',
+      cmdclass={'build_ext': build_ext},
+      ext_modules=ext_modules,
+      test_suite='test')
